23be3b3ef729407d1826076c00d3eada29a5ddbb
Mui Kai En <muikaien1@gmail.com>
2019-01-21 23:00:07 +0800

    Remove dead code

--- assets/js/councils.js
+++ assets/js/councils.js
@@ -12,30 +12,3 @@ let updateWindow = debounce(function(positionY) {
 window.addEventListener('scroll', function() {
     updateWindow(window.scrollY)
 });
-
-// let councilElements = document.getElementsByClassName('council');
-// let councils = []
-// for (var i = councilElements.length - 1; i >= 0; i--) {
-//     councils.push({ tween: null, element: councilElements[i] })
-// }
-// console.log(councils)
-// for (var i = councils.length - 1; i >= 0; i--) {
-//     let council = councils[i]
-//     council.tween = new TweenLite.to(council.element, 
-//         5, 
-//         { 
-//             transform: "translateX(-90vw)", 
-//             ease: "linear",
-//             immediateRender: true,
-//             onComplete: function() {
-//                 council.tween.restart();
-//             } 
-//         });
-//     council.tween.play()
-//     council.element.addEventListener("mouseover", () => {
-//         council.tween.pause();
-//     });
-//     council.element.addEventListener("mouseout", () => {
-//         council.tween.play();
-//     });
-// }
