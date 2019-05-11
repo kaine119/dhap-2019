b59481b06ea1ee910f7755235365cd057bd89a19
Mui Kai En <muikaien1@gmail.com>
2019-05-11 08:53:12 +0800

    Add styling for password-checking

--- assets/js/delegate_resources.js
+++ assets/js/delegate_resources.js
@@ -1,10 +1,12 @@
 ---
 ---
 
+
+let form = document.querySelector("#password-form");
+
 form.addEventListener("submit", function(e) {
   let expando = document.querySelector("#expando");
 
-  let form = document.querySelector("#password-form");
   let password = document.querySelector("#password")
   let message = document.querySelector("#password-form__flash");
   
@@ -15,9 +17,14 @@ form.addEventListener("submit", function(e) {
                 .map(val => val.toString(16))
                 .join('');
     if (hash == "{{ site.resources_password }}") {
+      message.classList.remove("wrong");
       expando.classList.add("open");
     } else {
-      
+      password.classList.remove("wrong");
+      setTimeout(function() {
+        password.classList.add("wrong");
+      }, 10);
+      message.classList.add("wrong");
     }
   });
 
