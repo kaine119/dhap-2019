3e0cb197f800d209c03ff8f4f294d8f5aa3785ed
Mui Kai En <muikaien1@gmail.com>
2019-05-11 08:21:51 +0800

    Add password-checking code for frontend

--- assets/js/delegate_resources.js
+++ assets/js/delegate_resources.js
@@ -1,17 +1,25 @@
 ---
 ---
-let expando = document.querySelector("#expando");
-
-let form = document.querySelector("#password-form");
-let password = document.querySelector("#password")
 
 form.addEventListener("submit", function(e) {
-  // if (password.value == "{{ site.resources_password }}") {
-  //   console.log("come on in!");
-  // } else {
-  //   console.log("go away")
-  // }
+  let expando = document.querySelector("#expando");
 
-  expando.classList.add("open");
+  let form = document.querySelector("#password-form");
+  let password = document.querySelector("#password")
+  let message = document.querySelector("#password-form__flash");
+  
+  window.crypto.subtle.digest("SHA-256", 
+        (new TextEncoder()).encode(password.value))
+  .then(function(digest) {
+    let hash = Array.from(new Uint8Array(digest))
+                .map(val => val.toString(16))
+                .join('');
+    if (hash == "{{ site.resources_password }}") {
+      expando.classList.add("open");
+    } else {
+      
+    }
+  });
+
   e.preventDefault();
 });
